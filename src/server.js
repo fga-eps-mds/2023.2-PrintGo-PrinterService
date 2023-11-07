48279a28249edb556eb4de83347e1843cf48a50b
julisous <julia.farias2000@hotmail.com>
2023-11-06 22:06:36 -0300

    :bulb: (#165) Adicionando primeira versão do server

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+//CORS habilitation
+app.use(cors());
+
+//JSON and URL encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//database
+let database = {
+  printers: [],
+};
+
+//regiter printer route
+app.post("/register-printer", (req, res) => {
+  const { name, model, ip } = req.body;
+  if (!name || !model || !ip) {
+    return res
+      .status(400)
+      .send({ message: "Nome, modelo e IP são obrigatórios" });
+  }
+  //New object
+  const newPrinter = {
+    id: database.printers.length + 1,
+    name,
+    model,
+    ip,
+  };
+
+  // Add database
+  database.printers.push(newPrinter);
+
+  //response
+  res.status(201).send(newPrinter);
+});
