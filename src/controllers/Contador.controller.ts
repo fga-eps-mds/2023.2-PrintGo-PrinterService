ee4ba01f31b4daecf385e71cb82682d17fa1864b
DaviMatheus <matdavimat@gmail.com>
2023-12-03 23:43:04 -0300

    🐴colocando_controllers

---
+++ src/controllers/Contador.controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { prisma } from '../database';
+import { ContadorCreateInput } from '../types/Contador.type';
+
+export default {
+    async createContador(request: Request, response: Response) {
+        try {
+            const {
+                contadorCopiasPB,
+                contadorImpressoesPB,
+                numeroSerie,
+                contadorImpressoesColoridas,
+                contadorCopiasColoridas, 
+                contadorGeral,
+                dataHoraEmissaoRelatorio, 
+                
+            } = request.body as ContadorCreateInput;
+
+            const impressoraExist = await prisma.impressora.findUnique({ where: { numeroSerie } });
+
+            if (!impressoraExist) {
+                return response.status(400).json({
+                    error: true,
+                    message: 'Erro: Impressora já existe!'
+                });
+            }
+
+            const contador = await prisma.contador.create({
+                data: {
+
+                    contadorCopiasPB,
+                    contadorImpressoesPB,
+                    numeroSerie,
+                    contadorImpressoesColoridas,
+                    contadorCopiasColoridas, 
+                    contadorGeral,
+                    dataHoraEmissaoRelatorio, 
+                }
+            });
+
+            return response.status(201).json({
+                message: 'Sucesso: Contador Manual cadastrado com sucesso!',
+                data: contador
+            });
+
+        } catch (error) {
+            return response.json({ error: true, message: error.message });
+        }
+    },
+};
