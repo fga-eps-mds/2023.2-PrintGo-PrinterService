bc07f8fc0a5228437abf873cc83b04c7c1c70535
mibasFerraz <lucasllff@gmail.com>
2023-12-05 22:28:11 -0300

    :test:(#203): adicionando teste para padrao

---
+++ tests/padrao.controller.spec.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import { server } from '../src/server';
+
+describe('POST /padrao/create', () => {
+  it('should create a new padrao and return a 201 status', async () => {
+    const padraoData = {
+      tipo: 'impressora',
+      marca: 'exemplo_marca',
+      modelo: 'exemplo_modelo',
+      numeroSerie: 'exemplo_numero_serie',
+      versaoFirmware: 'exemplo_versao_firmware',
+      tempoAtivoSistema: "2023-12-01T15:30:00Z",
+      totalDigitalizacoes: "500",
+      totalCopiasPB: "1000",
+      totalCopiasColoridas: "200",
+      totalImpressoesPb: "800",
+      totalImpressoesColoridas: "150",
+      totalGeral: "950",
+      enderecoIp: '192.168.1.2',
+    };
+
+    const response = await request(server)
+      .post('/padrao/create')
+      .send(padraoData);
+
+    expect(response.status).toBe(201);
+    expect(response.body.message).toBe('Sucesso: padrao cadastrada com sucesso!');
+    expect(response.body.data).toHaveProperty('id');
+  });
+});
