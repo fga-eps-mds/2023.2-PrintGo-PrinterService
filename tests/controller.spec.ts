672902ca8be7a842f8824da110fa9882a4c95109
mibasFerraz <lucasllff@gmail.com>
2023-12-05 22:19:56 -0300

    :test:(#203): adicionando mais teste para controller

--- tests/controller.spec.ts
+++ tests/controller.spec.ts
@@ -24,4 +24,27 @@ describe('POST /contador/create', () => {
     expect(response.body.message).toBe('Sucesso: Contador Manual cadastrado com sucesso!');
     expect(response.body.data).toHaveProperty('id'); // Verifica se o ID está presente no objeto de resposta
   });
+
+  it('should return a 400 status if impressora does not exist', async () => {
+    // Dados de exemplo para o corpo da requisição
+    const contadorData = {
+      contadorCopiasPB: "500",
+      contadorImpressoesPB: "1000",
+      numeroSerie: "impressora_inexistente", // Use um número de série que não exista
+      contadorImpressoesColoridas: "150",
+      contadorCopiasColoridas: "100",
+      contadorGeral: "1200",
+      dataHoraEmissaoRelatorio: "2023-12-01T14:45:00Z"
+    };
+
+    // Realizar a requisição POST
+    const response = await request(server)
+      .post('/contador/create')
+      .send(contadorData);
+
+    // Verificar se a resposta está correta
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe(true);
+    expect(response.body.message).toBe('Erro: Impressora já existe!');
+  });
 });
